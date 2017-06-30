b59e84d82a31c81688b22b3ba80faceec22b009d
Angel Hernandez <angherq@outlook.com>
2017-06-30 16:04:10 -0500

    Procfile for Heroku

--- resources/views/elevator.blade.php
+++ resources/views/elevator.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="Elevator test">
     <meta name="author" content="Angel H">
 
-    <title>Bare - Start Bootstrap Template</title>
+    <title>Elevator</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
